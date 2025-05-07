9421c46f08ac52817d8fc33d05fdc9789c3a448c
Kangjae Choi <awpe89@gmail.com>
2025-05-07 15:39:44 +0900

    feat: add Member test, clean-up code

--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -1,39 +1,36 @@
 package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
-
-import java.util.HashMap;
 import java.util.*;
+import java.util.HashMap;
 
 public class MemoryMemberRepository implements MemberRepository {
-    private static Map<Long, Member> store = new HashMap();
-    private static long sequence = 0L;
+  private static Map<Long, Member> store = new HashMap();
+  private static long sequence = 0L;
 
-    @Override
-    public Member save(Member member) {
-        member.setId(++sequence);
-        store.put(member.getId(), member);
-        return member;
-    }
+  @Override
+  public Member save(Member member) {
+    member.setId(++sequence);
+    store.put(member.getId(), member);
+    return member;
+  }
 
-    @Override
-    public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
-    }
+  @Override
+  public Optional<Member> findById(Long id) {
+    return Optional.ofNullable(store.get(id));
+  }
 
-    @Override
-    public Optional<Member> findByName(String name) {
-        return store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
-    }
+  @Override
+  public Optional<Member> findByName(String name) {
+    return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+  }
 
-    @Override
-    public List<Member> findAll() {
-        return new ArrayList<>(store.values());
-    }
+  @Override
+  public List<Member> findAll() {
+    return new ArrayList<>(store.values());
+  }
 
-    public void clearStore() {
-        store.clear();
-    }
+  public void clearStore() {
+    store.clear();
+  }
 }
